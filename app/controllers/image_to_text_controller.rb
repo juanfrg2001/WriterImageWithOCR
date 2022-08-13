3a0dd19d7a44e6f81aef54683752ca864bb60288
Juan Ruiz <juanfrg2001@gmail.com>
2022-08-13 17:16:51 -0500

    Fixes model methods

--- app/controllers/image_to_text_controller.rb
+++ app/controllers/image_to_text_controller.rb
@@ -3,7 +3,12 @@ class ImageToTextController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        @image_text = ImageToText.image_text("assets/prueba1.jpg")
+        image_url = params[:image_url]
+        if image_url.present?
+            @image_text = ImageToText.image_text(image_url)
+        else
+            @image_text = "Texto"
+        end
         render :json => { 
             :text => @image_text,
         }.to_json
