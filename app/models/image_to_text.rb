a23210edd17647ef6004a490827b1b581c84c980
Juan Ruiz <juanfrg2001@gmail.com>
2022-08-13 18:04:44 -0500

    Modifies

--- app/models/image_to_text.rb
+++ app/models/image_to_text.rb
@@ -1,10 +1,10 @@
 class ImageToText < ApplicationRecord
     
     def self.image_text(path)
-        File.open("/tmp/test_file.jpg", "wb") do |f| 
+        File.open("tmp/test_file.jpg", "wb") do |f| 
             f.write HTTParty.get(path).body
         end
-        image = RTesseract.new('/tmp/test_file.jpg')
+        image = RTesseract.new('tmp/test_file.jpg')
         image_text = image.to_s
         if image_text != " \n\f"
             return Nokogiri::HTML(image_text).text
