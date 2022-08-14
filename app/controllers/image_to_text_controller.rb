33750fd3853d32e3f4423d6234688b0e3743d159
Juan Ruiz <juanfrg2001@gmail.com>
2022-08-13 19:36:52 -0500

    Fixes names in methods

--- app/controllers/image_to_text_controller.rb
+++ app/controllers/image_to_text_controller.rb
@@ -3,15 +3,15 @@ class ImageToTextController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        image_url = params[:image_url]
-        if image_url.present?
-            @image_text = ImageToText.image_text(image_url)
+        encoded64_image = params[:encoded64_image]
+        if encoded64_image.present?
+            @image_text = ImageToText.image_text(encoded64_image)
         else
-            @image_text = "Texto"
+            @image_text = "encoded64_image parameter not found"
         end
-        render :json => { 
-            :text => @image_text,
-        }.to_json
+        respond_to do |format|
+            format.json { render :json => { :text => @image_text }.to_json  }
+        end
     end
 
 end
